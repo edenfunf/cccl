ecbef17d0e3b9efd85666b7d1a154788e1a3ad33
gonidelis <ggonidelis@nvidia.com>
2026-03-19 17:05:21 -0700

    Improve DeviceScan env test coverage and clean up duplicate doc comments
      - Add default environment test for ExclusiveScan with FutureValue
      - Add not_guaranteed determinism test for ExclusiveScan
      - Remove duplicate in-place precondition from env overload descriptions (already in Preconditions section)
      - Remove @devicestorage from env overloads (no temp storage parameter)

--- cub/cub/device/device_scan.cuh
+++ cub/cub/device/device_scan.cuh
@@ -331,10 +331,6 @@ struct DeviceScan
   //!   addition of floating-point types). Results for pseudo-associative
   //!   operators may vary from run to run. Additional details can be found in
   //!   the @lookback description.
-  //! - When ``d_in`` and ``d_out`` are equal, the scan is performed in-place.
-  //!   The range ``[d_in, d_in + num_items)`` and ``[d_out, d_out + num_items)``
-  //!   shall not overlap in any other way.
-  //! - @devicestorage
   //!
   //! Preconditions
   //! +++++++++++++
@@ -627,7 +623,6 @@ struct DeviceScan
   //! - When ``d_in`` and ``d_out`` are equal, the scan is performed in-place. The
   //!   range ``[d_in, d_in + num_items)`` and ``[d_out, d_out + num_items)``
   //!   shall not overlap in any other way.
-  //! - @devicestorage
   //!
   //! Snippet
   //! +++++++++++++++++++++++++++++++++++++++++++++
@@ -1080,9 +1075,6 @@ struct DeviceScan
   //!   addition of floating-point types). Results for pseudo-associative
   //!   operators may vary from run to run. Additional details can be found in
   //!   the @lookback description.
-  //! - When ``d_in`` and ``d_out`` are equal, the scan is performed in-place. The
-  //!   range ``[d_in, d_in + num_items)`` and ``[d_out, d_out + num_items)``
-  //!   shall not overlap in any other way.
   //!
   //! Preconditions
   //! +++++++++++++
@@ -1364,9 +1356,6 @@ struct DeviceScan
   //!   addition of floating-point types). Results for pseudo-associative
   //!   operators may vary from run to run. Additional details can be found in
   //!   the @lookback description.
-  //! - When ``d_in`` and ``d_out`` are equal, the scan is performed in-place.
-  //!   The range ``[d_in, d_in + num_items)`` and ``[d_out, d_out + num_items)``
-  //!   shall not overlap in any other way.
   //!
   //! Preconditions
   //! +++++++++++++
